6c3177d86868feb530a0778b9f829ae21c0b7167
matthieusb <sauboua.beneluz.matthieu@gmail.com>
2017-01-14 23:56:33 +0100

    Added and tested merge sort

--- Source/Python/python_sort/sorting_algo/sorting.py
+++ Source/Python/python_sort/sorting_algo/sorting.py
@@ -13,8 +13,8 @@ def bubble_sort(listToSort, operator):
     :type operator: :class:`operator`
     """
     if listToSort:
-        listToReturnSorted = list(listToSort)  # copying the input list
-        lengthMinusOne = len(listToReturnSorted) - 1
+        listToReturn = list(listToSort)  # copying the input list
+        lengthMinusOne = len(listToReturn) - 1
 
         swappedFlag = True
         i = 0
@@ -22,20 +22,20 @@ def bubble_sort(listToSort, operator):
             swappedFlag = False
             j = 0
             while j < lengthMinusOne:
-                if operator(listToReturnSorted[j + 1], listToReturnSorted[j]):
+                if operator(listToReturn[j + 1], listToReturn[j]):
                     general_utilities.\
-                        swap_list_elements(listToReturnSorted, j, j + 1)
+                        swap_list_elements(listToReturn, j, j + 1)
                     swappedFlag = True
                 j = j + 1
             i = i + 1
 
-        return listToReturnSorted
+        return listToReturn
     else:
         return []
 
 
 def insertion_sort(listToSort, operator):
-    """Sorts the given list using insertions algorithm and given operator.
+    """Sorts the given list using insertion algorithm and given operator.
     Returns a list
 
     :param listToSort: The list to check
@@ -45,22 +45,103 @@ def insertion_sort(listToSort, operator):
     """
 
     if listToSort:
-        listToReturnSorted = list(listToSort)
-        listLength = len(listToReturnSorted)
+        listToReturn = list(listToSort)
+        listLength = len(listToReturn)
         for i in range(0, listLength):
-            valueToInsert = listToReturnSorted[i]
+            valueToInsert = listToReturn[i]
             indexToInsert = i
 
             while (
                 indexToInsert > 0 and
-                operator(valueToInsert, listToReturnSorted[indexToInsert - 1])
+                operator(valueToInsert, listToReturn[indexToInsert - 1])
             ):
-                listToReturnSorted[indexToInsert] = \
-                    listToReturnSorted[indexToInsert - 1]
+                listToReturn[indexToInsert] = \
+                    listToReturn[indexToInsert - 1]
                 indexToInsert = indexToInsert - 1
 
-            listToReturnSorted[indexToInsert] = valueToInsert
+            listToReturn[indexToInsert] = valueToInsert
 
-        return listToReturnSorted
+        return listToReturn
     else:
         return []
+
+
+def selection_sort(listToSort, operator):
+    """Sorts the given list using selection algorithm and given operator.
+    Returns a list
+
+    :param listToSort: The list to check
+    :type listToSort: :class:`list`
+    :param operator: operator.le for asc, operator.ge for desc
+    :type operator: :class:`operator`
+    """
+    if listToSort:
+        listToReturn = list(listToSort)
+        listLength = len(listToReturn)
+        listLengthMinusOne = listLength - 1
+
+        for i in range(0, listLengthMinusOne):
+            minIndex = i
+
+            # Checking if the element is minimum of the whole list
+            for j in range(i + 1, listLength):
+                if operator(listToReturn[j], listToReturn[minIndex]):
+                    minIndex = j
+
+            if minIndex != i:
+                general_utilities.swap_list_elements(
+                    listToReturn, minIndex, i)
+
+        return listToReturn
+    else:
+        return[]
+
+
+def merge_sort(listToSort, operator):
+    if listToSort:
+        return merge_do_algo(listToSort, operator)
+    else:
+        return[]
+
+
+def merge_do_algo(listToSort, operator):
+    listSize = len(listToSort)
+
+    if listSize == 1:
+        return listToSort
+    else:
+        listFirstHalf = listToSort[:listSize/2]
+        listSecondHalf = listToSort[listSize/2:]
+
+        listFirstHalf = merge_do_algo(listFirstHalf, operator)
+        listSecondHalf = merge_do_algo(listSecondHalf, operator)
+
+    return merge_list_halves(listFirstHalf, listSecondHalf, operator)
+
+
+def merge_list_halves(listOne, listTwo, operator):
+    listResult = []
+
+    while (listOne and listTwo):
+        if operator(listTwo[0], listOne[0]):
+            listResult.append(listTwo[0])
+            listTwo.pop(0)
+        else:
+            listResult.append(listOne[0])
+            listOne.pop(0)
+
+    while listOne:
+        listResult.append(listOne.pop(0))
+
+    while listTwo:
+        listResult.append(listTwo.pop(0))
+
+    return listResult
+
+
+def shell_sort(listToSort, operator):
+    return[]
+
+
+def quick_sort(listToSort, operator):
+    return[]
--- Source/Python/python_sort/tests/test_sorting.py
+++ Source/Python/python_sort/tests/test_sorting.py
@@ -22,6 +22,30 @@ class TestEmptyListSort(object):
                     self.data.emptyList, operator.le
                     ), operator.le)
 
+    def test_selectionSort_emptyList(self):
+        assert not general_utilities.\
+            is_list_sorted(sorting.selection_sort(
+                    self.data.emptyList, operator.le
+                    ), operator.le)
+
+    def test_mergeSort_emptyList(self):
+        assert not general_utilities.\
+            is_list_sorted(sorting.merge_sort(
+                    self.data.emptyList, operator.le
+                    ), operator.le)
+
+    def test_shellSort_emptyList(self):
+        assert not general_utilities.\
+            is_list_sorted(sorting.shell_sort(
+                    self.data.emptyList, operator.le
+                    ), operator.le)
+
+    def test_quickSort_emptyList(self):
+        assert not general_utilities.\
+            is_list_sorted(sorting.quick_sort(
+                    self.data.emptyList, operator.le
+                    ), operator.le)
+
 
 class TestBubbleSort(object):
     parameters = datasets.Parameters()
@@ -80,3 +104,119 @@ class TestInsertionSort(object):
                             is_list_sorted(sorting.insertion_sort(
                                 currentList, currentOp
                                 ), currentOp)
+
+
+class TestSelectionSort(object):
+    parameters = datasets.Parameters()
+    data = datasets.SortingDataSets()
+
+    def test_selectionSort_simple_int_list(self):
+        assert general_utilities.\
+            is_list_sorted(sorting.selection_sort(
+                self.data.simpleMixedIntList1, operator.le
+                ), operator.le)
+
+        assert general_utilities.\
+            is_list_sorted(sorting.selection_sort(
+                self.data.simpleMixedIntList2, operator.ge
+                ), operator.ge)
+
+    def test_selectionSort_complex_generated_list_asc_desc(self):
+        for length in range(1, self.parameters.lengthRangeMax):
+            for minTest in range(self.parameters.minTestRange, 0):
+                for maxTest in range(0, self.parameters.maxTestRange):
+                    for currentOp in [operator.le, operator.ge]:
+                        currentList = general_utilities.\
+                            generate_random_int_list(length, minTest, maxTest)
+                        # Test for each length/minTest/maxTest/currentOp
+                        assert general_utilities.\
+                            is_list_sorted(sorting.selection_sort(
+                                currentList, currentOp
+                                ), currentOp)
+
+
+class TestMergeSort(object):
+    parameters = datasets.Parameters()
+    data = datasets.SortingDataSets()
+
+    def test_mergeSort_simple_int_list(self):
+        assert general_utilities.\
+            is_list_sorted(sorting.merge_sort(
+                self.data.simpleMixedIntList1, operator.le
+                ), operator.le)
+
+        assert general_utilities.\
+            is_list_sorted(sorting.merge_sort(
+                self.data.simpleMixedIntList2, operator.ge
+                ), operator.ge)
+
+    def test_mergeSort_complex_generated_list_asc_desc(self):
+        for length in range(1, self.parameters.lengthRangeMax):
+            for minTest in range(self.parameters.minTestRange, 0):
+                for maxTest in range(0, self.parameters.maxTestRange):
+                    for currentOp in [operator.le, operator.ge]:
+                        currentList = general_utilities.\
+                            generate_random_int_list(length, minTest, maxTest)
+                        # Test for each length/minTest/maxTest/currentOp
+                        assert general_utilities.\
+                            is_list_sorted(sorting.merge_sort(
+                                currentList, currentOp
+                                ), currentOp)
+
+
+# class TestShellSort(object):
+#     parameters = datasets.Parameters()
+#     data = datasets.SortingDataSets()
+#
+#     def test_shellSort_simple_int_list(self):
+#         assert general_utilities.\
+#             is_list_sorted(sorting.shell_sort(
+#                 self.data.simpleMixedIntList1, operator.le
+#                 ), operator.le)
+#
+#         assert general_utilities.\
+#             is_list_sorted(sorting.shell_sort(
+#                 self.data.simpleMixedIntList2, operator.ge
+#                 ), operator.ge)
+#
+#     def test_shellSort_complex_generated_list_asc_desc(self):
+#         for length in range(1, self.parameters.lengthRangeMax):
+#             for minTest in range(self.parameters.minTestRange, 0):
+#                 for maxTest in range(0, self.parameters.maxTestRange):
+#                     for currentOp in [operator.le, operator.ge]:
+#                         currentList = general_utilities.\
+#                             generate_random_int_list(length, minTest, maxTest)
+#                         # Test for each length/minTest/maxTest/currentOp
+#                         assert general_utilities.\
+#                             is_list_sorted(sorting.shell_sort(
+#                                 currentList, currentOp
+#                                 ), currentOp)
+#
+#
+# class TestQuickSort(object):
+#     parameters = datasets.Parameters()
+#     data = datasets.SortingDataSets()
+#
+#     def test_quickSort_simple_int_list(self):
+#         assert general_utilities.\
+#             is_list_sorted(sorting.quick_sort(
+#                 self.data.simpleMixedIntList1, operator.le
+#                 ), operator.le)
+#
+#         assert general_utilities.\
+#             is_list_sorted(sorting.quick_sort(
+#                 self.data.simpleMixedIntList2, operator.ge
+#                 ), operator.ge)
+#
+#     def test_quickSort_complex_generated_list_asc_desc(self):
+#         for length in range(1, self.parameters.lengthRangeMax):
+#             for minTest in range(self.parameters.minTestRange, 0):
+#                 for maxTest in range(0, self.parameters.maxTestRange):
+#                     for currentOp in [operator.le, operator.ge]:
+#                         currentList = general_utilities.\
+#                             generate_random_int_list(length, minTest, maxTest)
+#                         # Test for each length/minTest/maxTest/currentOp
+#                         assert general_utilities.\
+#                             is_list_sorted(sorting.quick_sort(
+#                                 currentList, currentOp
+#                                 ), currentOp)
